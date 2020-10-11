8edb3c47173a888d4ec089baa686bd75dc245b56
eminaydin <eminaydin93@yahoo.com>
2020-10-11 18:13:02 +0200

    Main info about the team

--- src/components/BasicTeamInformation.js
+++ src/components/BasicTeamInformation.js
@@ -1,30 +1,36 @@
-import React from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Grid, Header, Image } from "semantic-ui-react";
 
 const BasicTeamInformation = () => {
-  return (
-    <Grid columns={3} divided>
-      <Grid.Row>
-        <Grid.Column>
-          <Image src="/images/wireframe/media-paragraph.png" />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src="/images/wireframe/media-paragraph.png" />
-        </Grid.Column>
-        <Grid.Column>
-          <Image src="/images/wireframe/media-paragraph.png" />
-        </Grid.Column>
-      </Grid.Row>
+  const state = useSelector((state) => state.teamReducer.teamInfo);
 
+  const findTheCoach = () => {
+    let coach = state?.squad.find(({ role }) => role === "COACH").name;
+    return coach;
+  };
+  return (
+    <Grid columns={5} divided>
       <Grid.Row>
-        <Grid.Column>
-          <Image src="/images/wireframe/media-paragraph.png" />
+        <Grid.Column className="logoColumn">
+          <Image src={state?.crestUrl} size="small" />
+          <Header textAlign="center"> {state?.name}</Header>
         </Grid.Column>
-        <Grid.Column>
-          <Image src="/images/wireframe/media-paragraph.png" />
+        <Grid.Column verticalAlign="middle">
+          <Header>Stadium</Header>
+          <Header> {state?.venue}</Header>
         </Grid.Column>
-        <Grid.Column>
-          <Image src="/images/wireframe/media-paragraph.png" />
+        <Grid.Column verticalAlign="middle">
+          <Header>Club Colors</Header>
+          <Header> {state?.clubColors}</Header>
+        </Grid.Column>
+        <Grid.Column verticalAlign="middle">
+          <Header>Coach</Header>
+          <Header> {findTheCoach()}</Header>
+        </Grid.Column>
+        <Grid.Column verticalAlign="middle">
+          <Header>Country</Header>
+          <Header> {state?.area.name}</Header>
         </Grid.Column>
       </Grid.Row>
     </Grid>

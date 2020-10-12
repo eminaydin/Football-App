f23054fb518f3efcf1b69793f4de10a4066fe436
eminaydin <eminaydin93@yahoo.com>
2020-10-12 19:45:54 +0200

    Fix missing flags

--- src/components/TeamSquad.js
+++ src/components/TeamSquad.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Moment from "react-moment";
 import { useSelector } from "react-redux";
 import { Flag, Table, Image } from "semantic-ui-react";
 import moment from "moment";
@@ -11,7 +10,6 @@ const TeamSquad = () => {
     <Table celled selectable>
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell>#</Table.HeaderCell>
           <Table.HeaderCell>Position</Table.HeaderCell>
           <Table.HeaderCell>Player</Table.HeaderCell>
           <Table.HeaderCell>Date Of Birth / Age</Table.HeaderCell>
@@ -20,26 +18,37 @@ const TeamSquad = () => {
       </Table.Header>
 
       <Table.Body>
-        {players?.map(
-          ({ dateOfBirth, name, nationality, position, shirtNumber, id }) => {
-            var formattedDate = moment(dateOfBirth).format("MMM D,YYYY");
-            var age = moment().diff(dateOfBirth, "years");
+        {players?.map(({ dateOfBirth, name, nationality, position, id }) => {
+          var formattedDate = moment(dateOfBirth).format("MMM D,YYYY");
+          var age = moment().diff(dateOfBirth, "years");
 
-            return (
-              <Table.Row key={id}>
-                <Table.Cell>{shirtNumber}</Table.Cell>
-                <Table.Cell>{position}</Table.Cell>
-                <Table.Cell>{name}</Table.Cell>
-                <Table.Cell>
-                  {formattedDate} ({age})
-                </Table.Cell>
-                <Table.Cell>
-                  <Flag name={nationality.toLowerCase()} />
-                </Table.Cell>
-              </Table.Row>
-            );
-          }
-        )}
+          return (
+            <Table.Row key={id}>
+              <Table.Cell>{position}</Table.Cell>
+              <Table.Cell>{name}</Table.Cell>
+              <Table.Cell>
+                {formattedDate} ({age})
+              </Table.Cell>
+              <Table.Cell>
+                <Flag
+                  name={
+                    nationality.toLowerCase() === "côte d’ivoire"
+                      ? "cote divoire"
+                      : nationality.toLowerCase() === "north macedonia"
+                      ? "macedonia"
+                      : nationality.toLowerCase() === "bosnia and herzegovina"
+                      ? "bosnia"
+                      : nationality.toLowerCase() === "northern ireland"
+                      ? "ireland"
+                      : nationality.toLowerCase() === `korea republic`
+                      ? "south korea"
+                      : nationality.toLowerCase()
+                  }
+                />
+              </Table.Cell>
+            </Table.Row>
+          );
+        })}
       </Table.Body>
     </Table>
   );

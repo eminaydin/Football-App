af7725e9104aab6c3a4d80cfc975334e18def35f
eminaydin <eminaydin93@yahoo.com>
2020-10-14 23:24:44 +0200

    Display club icons

--- src/components/MatchSlider.js
+++ src/components/MatchSlider.js
@@ -1,47 +1,52 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { Fragment } from "react";
 import HorizontalScroll from "react-scroll-horizontal";
-import { Card, Grid, Image } from "semantic-ui-react";
-import src from "../assets/football.jpg";
-import { fetchFixture, fetchTeam } from "../helpers/fetchFunctions";
-const MatchSlider = () => {
-  const state = useSelector((state) => state.leagueReducer.fixture);
-  const team = useSelector((state) => state.teamReducer.teamInfo);
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    fetchFixture("PL", dispatch);
-  }, []);
+const MatchSlider = ({ leagueState }) => {
+  const fixture = leagueState?.fixture;
+  const standings = leagueState?.league?.standings[0]?.table;
+
   return (
     <HorizontalScroll className="scoresDiv">
       <div className="scoresTeams">
-        {state?.map((team) => {
-          let homeTeamId = team.homeTeam.id;
-          let awayTeamId = team.awayTeam.id;
-          return (
-            <Fragment>
-              <div className="homeTeam">
-                <div className="homeTeamLogo">
-                  <img src="https://media03.tr.beinsports.com/img/teams/1/62.png" />
+        {console.log(leagueState)}
+        {standings != null &&
+          fixture?.map((match) => {
+            let homeTeam = standings?.find(
+              (club) => club.team.id === match.homeTeam.id
+            );
+            let awayTeam = standings?.find(
+              (club) => club.team.id === match.awayTeam.id
+            );
+            /*      console.log(homeTeam);
+            console.log(awayTeam); */
+            return (
+              <Fragment>
+                <div className="homeTeam">
+                  <div className="homeTeamLogo">
+                    <img src={homeTeam?.team?.crestUrl} />
+                  </div>
+                  <div className="homeScore">
+                    {match.score.fullTime.homeTeam}
+                  </div>
                 </div>
-                <div className="homeScore">{team.score.fullTime.homeTeam}</div>
-              </div>
-              <div className="seperatorTeam">
-                <div className="seperatorTeamLogo">
-                  <img />
+                <div className="seperatorTeam">
+                  <div className="seperatorTeamLogo">
+                    <img />
+                  </div>
+                  <div className="seperatorName"></div>
+                  <div className="seperatorScore"> : </div>
                 </div>
-                <div className="seperatorName"></div>
-                <div className="seperatorScore"> : </div>
-              </div>
-              <div className="awayTeam">
-                <div className="awayTeamLogo">
-                  <img src="https://media03.tr.beinsports.com/img/teams/1/62.png" />
+                <div className="awayTeam">
+                  <div className="awayTeamLogo">
+                    <img src={awayTeam?.team?.crestUrl} />
+                  </div>
+                  <div className="awayScore">
+                    {match.score.fullTime.awayTeam}
+                  </div>
                 </div>
-                <div className="awayScore">{team.score.fullTime.awayTeam}</div>
-              </div>
-            </Fragment>
-          );
-        })}
+              </Fragment>
+            );
+          })}
         ;
       </div>
     </HorizontalScroll>
